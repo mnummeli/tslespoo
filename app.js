7474717c3b812413727dd528502dd9bf8007b3a8
Mikko Nummelin <mnummeli@gmail.com>
2019-02-05 22:06:19 +0200

    Järjestön nimi.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const title = 'Työväen sivistysliiton Espoon ja Kauniaisten opintopiiri';
+const title = 'Työväen sivistysliiton Espoon ja Kauniaisten opintojärjestö r.y.';
 document.title = title;
 $('h1#title').html(title);
