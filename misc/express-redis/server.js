8f9b750fa36c76e171ef93618c3330d49fd6b6df
Mikko Nummelin <mnummeli@gmail.com>
2020-11-01 19:26:42 +0200

    WIP: Docker ja Docker-compose

--- misc/express-redis/server.js
+++ misc/express-redis/server.js
@@ -9,14 +9,24 @@ const app = express();
 const format = require('date-format');
 const os = require('os');
 const path = require('path');
+const bunyan = require('bunyan');
+const logger = bunyan.createLogger({name: 'server'});
 
 const PORT = process.env.EXPRESS_PORT || 3000;
 
+logger.info(`Application starting on server ${os.hostname()}.`);
+
 app.set('view engine', 'pug');
+
+app.post('/crash', () => {
+    logger.fatal(`Application is going to crash on server ${os.hostname()}: ${new Error('Bang')}`);
+    process.exit(1);
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
+    logger.info(`Received ${req.method} request to path ${req.path} on server ${os.hostname()} .`);
     const ts = new Date();
     res.render('index', {
         title: "Palvelimen tiedot",
@@ -29,5 +39,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Sovellus vastaa portissa ${PORT}.`);
+    logger.info(`Application responds in http://${os.hostname()}:${PORT}/ .`);
 });
