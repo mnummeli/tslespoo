3fd552127b4cef83553a97cab3cd90c191c5eb8f
Mikko Nummelin <mnummeli@gmail.com>
2020-11-01 22:24:20 +0200

    Improvements

--- misc/express-redis/server.js
+++ misc/express-redis/server.js
@@ -11,8 +11,16 @@ const os = require('os');
 const path = require('path');
 const bunyan = require('bunyan');
 const logger = bunyan.createLogger({name: 'server'});
+const redis = require('redis');
 
 const PORT = process.env.EXPRESS_PORT || 3000;
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const redisClient = redis.createClient({
+    host: REDIS_HOST,
+    port: REDIS_PORT
+});
 
 logger.info(`Application starting on server ${os.hostname()}.`);
 
@@ -28,13 +36,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res) => {
     logger.info(`Received ${req.method} request to path ${req.path} on server ${os.hostname()} .`);
     const ts = new Date();
-    res.render('index', {
-        title: "Palvelimen tiedot",
-        hostname: os.hostname(),
-        method: req.method,
-        path: req.path,
-        current_date: format('dd.MM.yyyy', ts),
-        current_time: format('hh:mm:ss', ts)
+    redisClient.incr('visits', visits => {
+        res.render('index', {
+            title: "Palvelimen tiedot",
+            hostname: os.hostname(),
+            method: req.method,
+            path: req.path,
+            current_date: format('dd.MM.yyyy', ts),
+            current_time: format('hh:mm:ss', ts),
+            visits: visits
+        });
     });
 });
 
