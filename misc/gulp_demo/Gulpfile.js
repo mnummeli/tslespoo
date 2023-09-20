a346ee1e908dca26e79c988a6857aa42675a86ee
Mikko Ilmari Nummelin <mnummeli@gmail.com>
2023-09-20 21:53:09 +0300

    createFile

--- misc/gulp_demo/Gulpfile.js
+++ misc/gulp_demo/Gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import { rm } from 'node:fs';
+import { Readable } from 'node:stream';
 import gulp from 'gulp';
+import Vinyl from 'vinyl';
 
 function clean(cb) {
     rm('dist/', {force: true, recursive: true}, cb);
@@ -11,8 +13,24 @@ function copyFiles() {
     return gulp.src('src/*').pipe(gulp.dest('dist/'));
 }
 
-function createFile(cb) {
-    cb();
+function createFile() {
+    let finished = false;
+    const readableStream = new Readable({
+        read: function (n) {
+            if (!finished) {
+                const vinyl = new Vinyl({
+                    path: 'teksti2.txt',
+                    contents: Buffer.from('Joka ei koodata osaa, ei sen syömänkään pidä!')
+                });
+                this.push(vinyl);
+                finished = true;
+            } else {
+                this.push(null);
+            }
+        },
+        objectMode: true
+    });
+    return readableStream.pipe(gulp.dest('dist/'));
 }
 
 export {clean};
